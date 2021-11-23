ec3e2d29ac209d298fc7377ffe4ba24dce7c7876
Egor Shamanskiy <68269553+gor2d@users.noreply.github.com>
2021-11-23 15:13:17 +0400

    rename betatravel

--- charts/atlas.sh
+++ charts/atlas.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 helm upgrade --install --create-namespace \
-  -n alfatravel \ #change "alfatravel" on your namespace
+  -n betatravel \ #change "betatravel" on your namespace
   -f atlas_values.yaml \ #values with helm charts 
-  alfaatlas \ # name helmcharts
+  betaatlas \ # name helmcharts
   ./atlas # path to directory charts
