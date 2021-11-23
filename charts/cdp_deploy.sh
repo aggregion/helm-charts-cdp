99a03a05570ef0420abd592ee1799aaeb6704889
Egor Shamanskiy <68269553+gor2d@users.noreply.github.com>
2021-11-23 15:14:44 +0400

    rename betatravel

--- charts/cdp_deploy.sh
+++ charts/cdp_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 helm upgrade --install --create-namespace \
-  -n alfatravel \ #change "alfatravel" on your namespace
+  -n betatravel \ #change "betatravel" on your namespace
   -f cdp_values.yaml \ #values with helm charts
-  alfa \ # name helmcharts
+  beta \ # name helmcharts
   ./aggregion-cdp #path to directory charts
