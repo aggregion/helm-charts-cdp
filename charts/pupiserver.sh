6cffd9044dae618ecb5f5da874ba0d6ebcf82e16
Egor Shamanskiy <68269553+gor2d@users.noreply.github.com>
2022-01-17 17:39:12 +0400

    rename

--- charts/pupiserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-helm upgrade --install --create-namespace \
-  -n datalab \ 
-  -f pypisrv_values.yaml \ #values with helm charts 
-  pypisrv \ # name helmcharts
-  ./aggregion-pypiserver # path to directory charts
