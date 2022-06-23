ca83ac753b00c5dfb07d9a60cd04ca327674c4e8
gor2d <egor2d@gmail.com>
2022-06-23 12:56:54 +0300

    change gatekeeper

--- charts/cdp_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-helm upgrade --install --create-namespace \
-  -n betatravel \ #change "betatravel" on your namespace
-  -f cdp_values.yaml \ #values with helm charts
-  beta \ # name helmcharts
-  ./aggregion-cdp #path to directory charts
