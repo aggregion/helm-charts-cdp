102ba4e296d2107798c1eeda22e920685314e4ea
Egor Shamanskiy <68269553+gor2d@users.noreply.github.com>
2022-07-24 18:34:28 +0300

    edit comments

--- tools/scripts/postgres_backup.sh
+++ tools/scripts/postgres_backup.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 timestamp=$(date +%Y-%m-%d_%H-%M)
-mkdir -p /root/Backup_postgres/bckp`date +%Y-%m-%d_%H-%M`
+mkdir -p /root/Backup_postgres/bckp`date +%Y-%m-%d_%H-%M` #EDIT YOUR DIRECTORY
 pod=$(kubectl get pods -A | grep postgres | awk {'print$2'})
-ns=betatravel
+ns=betatravel #EDIT YOUR NAMESPACE
 kubectl -n $ns exec -it $pod -- sh -c "pg_dumpall -U postgres > /tmp/postgres.dump"
-kubectl cp $ns/$pod:/tmp/postgres.dump /root/Backup_postgres/bckp`date +%Y-%m-%d_%H-%M`/postgres.dump
+kubectl cp $ns/$pod:/tmp/postgres.dump /root/Backup_postgres/bckp`date +%Y-%m-%d_%H-%M`/postgres.dump #EDIT YOUR DIRECTORY
 sleep 50
 kubectl -n $ns exec -it $pod -- sh -c "rm -rf /tmp/*.dump"
