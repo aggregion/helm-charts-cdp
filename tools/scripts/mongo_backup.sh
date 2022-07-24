2831d117e7deee696075037620fcaf655bb15783
Egor Shamanskiy <68269553+gor2d@users.noreply.github.com>
2022-07-24 18:34:46 +0300

    Update mongo_backup.sh

--- tools/scripts/mongo_backup.sh
+++ tools/scripts/mongo_backup.sh
@@ -6,7 +6,7 @@ pod=$(kubectl get pods -A | grep mongo | awk {'print$2'})
 ns=betatravel #EDIT YOUR NAMESPACE
 kubectl -n $ns exec -it $pod -- sh -c "mongodump --db=dmp --gzip --archive=/tmp/backup_dmp.gzip"
 kubectl -n $ns exec -it $pod -- sh -c "mongodump --db=ds --gzip --archive=/tmp/backup_ds.gzip"
-kubectl cp $ns/$pod:/tmp/backup_dmp.gzip /root/Backup_mongo/bckp`date +%Y-%m-%d_%H-%M`/backup_dmp.gzip
-kubectl cp $ns/$pod:/tmp/backup_ds.gzip /root/Backup_mongo/bckp`date +%Y-%m-%d_%H-%M`/backup_ds.gzip
+kubectl cp $ns/$pod:/tmp/backup_dmp.gzip /root/Backup_mongo/bckp`date +%Y-%m-%d_%H-%M`/backup_dmp.gzip #EDIT YOUR DIRECTORY
+kubectl cp $ns/$pod:/tmp/backup_ds.gzip /root/Backup_mongo/bckp`date +%Y-%m-%d_%H-%M`/backup_ds.gzip #EDIT YOUR DIRECTORY
 sleep 60
 kubectl -n $ns exec -it $pod -- sh -c "rm -rf /tmp/*.gzip"
