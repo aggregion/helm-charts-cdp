2ff5cda970c7a3c872a851ca136046a87f451863
Alex Kheben <zbitname@users.noreply.github.com>
2022-09-28 14:48:22 +0300

    Update keycloak.sh

--- charts/keycloak.sh
+++ charts/keycloak.sh
@@ -24,12 +24,14 @@ helm upgrade --install --create-namespace --namespace $KUBE_NAMESPACE \
   --set auth.adminPassword=${KEYCLOAK_PASSWORD} \
   --set global.postgresql.auth.postgresPassword=${POSTGRESQL_PASSWORD} \
   --set ingress.hostname=${DEPLOY_HOSTNAME} \
+  --set ingress.enabled=true \
+  --set ingress.annotations."kubernetes.io/ingress.class"=nginx \
   --set "extraEnvVars[0].name=KEYCLOAK_PRODUCTION" \
   --set "extraEnvVars[0].value=\"true\"" \
-  --set "extraEnvVars[1].name=\"KEYCLOAK_LOG_OUTPUT\"" \
+  --set "extraEnvVars[1].name=KEYCLOAK_LOG_OUTPUT" \
   --set "extraEnvVars[1].value=json" \
-  --set "extraEnvVars[2].name=\"KEYCLOAK_LOG_LEVEL\"" \
+  --set "extraEnvVars[2].name=KEYCLOAK_LOG_LEVEL" \
   --set "extraEnvVars[2].value=DEBUG" \
-  --set "extraEnvVars[3].name=\"KEYCLOAK_EXTRA_ARGS\"" \
+  --set "extraEnvVars[3].name=KEYCLOAK_EXTRA_ARGS" \
   --set "extraEnvVars[3].value=\"--spi-login-protocol-openid-connect-legacy-logout-redirect-uri=true\"" \
   keycloak ./keycloak19
