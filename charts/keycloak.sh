4828c63ed391688fc66700c393de550fea4a896f
Alex Kheben <zbitname@users.noreply.github.com>
2022-09-28 14:34:56 +0300

    Update keycloak.sh

--- charts/keycloak.sh
+++ charts/keycloak.sh
@@ -2,12 +2,14 @@
 
 #helm dependency build aggregion-keycloak
 
-export DEPLOY_HOSTNAME=example.domain.ltd
+cd $(dirname $0)
+
+export DEPLOY_HOSTNAME=kc.example.domain.ltd
 export KUBE_NAMESPACE=keycloak-namespace
 
 # if keycloak already installed - use existing passwords
-export POSTGRESQL_PASSWORD=$(kubectl get secret --namespace "$KUBE_NAMESPACE" keyclock-chart-postgresql -o jsonpath="{.data.postgres-password}" | base64 --decode)
-export KEYCLOAK_PASSWORD=$(kubectl get secret --namespace "$KUBE_NAMESPACE" keyclock-chart-keycloak -o jsonpath="{.data.admin-password}" | base64 --decode)
+export POSTGRESQL_PASSWORD=$(kubectl get secret --namespace "$KUBE_NAMESPACE" keycloak-chart-postgresql -o jsonpath="{.data.postgres-password}" | base64 --decode)
+export KEYCLOAK_PASSWORD=$(kubectl get secret --namespace "$KUBE_NAMESPACE" keycloak-chart-keycloak -o jsonpath="{.data.admin-password}" | base64 --decode)
 
 # otherwise generete new one
 if [ "$POSTGRESQL_PASSWORD" == "" ]
@@ -17,8 +19,16 @@ export KEYCLOAK_PASSWORD=$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | 
 fi
 
 
-helm upgrade --install --create-namespace --namespace $KUBE_NAMESPACE keycloak aggregion-keycloak -f aggregion-keycloak/Values.yaml \
+helm upgrade --install --create-namespace --namespace $KUBE_NAMESPACE keycloak ./keycloak19 \
   --set keycloak.enabled=true \
   --set auth.adminPassword=${KEYCLOAK_PASSWORD} \
   --set global.postgresql.auth.postgresPassword=${POSTGRESQL_PASSWORD} \
-  --set ingress.hostname=kc.${DEPLOY_HOSTNAME}
+  --set ingress.hostname=${DEPLOY_HOSTNAME} \
+  --set "extraEnvVars[0].name=KEYCLOAK_PRODUCTION" \
+  --set "extraEnvVars[0].value=\"true\"" \
+  --set "extraEnvVars[1].name=\"KEYCLOAK_LOG_OUTPUT\"" \
+  --set "extraEnvVars[1].value=json" \
+  --set "extraEnvVars[2].name=\"KEYCLOAK_LOG_LEVEL\"" \
+  --set "extraEnvVars[2].value=DEBUG" \
+  --set "extraEnvVars[3].name=\"KEYCLOAK_EXTRA_ARGS\"" \
+  --set "extraEnvVars[3].value=\"--spi-login-protocol-openid-connect-legacy-logout-redirect-uri=true\"" \
