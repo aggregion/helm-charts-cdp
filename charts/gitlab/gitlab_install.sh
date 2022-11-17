d7461c07e53e925748ab45d9f301537e9e3b9157
zbitname <i@zbit.name>
2022-11-17 04:15:07 +0300

    fix(gitlab): shame
    Former-commit-id: 2c5fdc7c8b22a54a7d238505d86c70a6b01f5ea4

--- charts/gitlab/gitlab_install.sh
+++ charts/gitlab/gitlab_install.sh
@@ -5,19 +5,18 @@ cd $(dirname $0)
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 helm -n gitlab upgrade --install gitlab gitlab/gitlab \
-   --timeout 600s \
-   --set global.edition=ce \
-   --global.hosts.domain=gitlab.local \
-   --set certmanager-issuer.email=ENTET_YOUR_EMAIL@example.com \
-   --set gitlab-runner.runners.privileged=true \
-   --set global.hosts.https=false \
-   --set global.shell.port=2222 \
-   --set certmanager.installCRDs=false \
-   --set kas.enables=false \
-   --set nginx-ingress.enabled=false \
-   --set global.ingress.class=nginx \
-   --set global.certmanager.install=false \
-   --set prometheus.install=false \
-   --set gitlab-runner.install=false \
-   --set global.hosts.externalIP=ENTER_YOUR_IP
-
+  --timeout 600s \
+  --set certmanager-issuer.email=ENTET_YOUR_EMAIL@example.com \
+  --set certmanager.install=false \
+  --set certmanager.installCRDs=false \
+  --set gitlab-runner.install=false \
+  --set gitlab-runner.runners.privileged=true \
+  --set global.edition=ce \
+  --set global.hosts.domain=gitlab.local \
+  --set global.hosts.externalIP=ENTER_YOUR_IP
+  --set global.hosts.https=false \
+  --set global.ingress.class=nginx \
+  --set global.kas.enabled=false \
+  --set global.shell.port=2222 \
+  --set nginx-ingress.enabled=false \
+  --set prometheus.install=false \
