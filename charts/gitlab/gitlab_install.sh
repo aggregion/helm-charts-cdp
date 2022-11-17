34ab577a28ea53534307217a8375eabe389aacd3
zbitname <i@zbit.name>
2022-11-17 04:24:13 +0300

    fix(gitlab): shame
    Former-commit-id: bd7f7c813b09fb6357c2da5355db0ba12b7e7305

--- charts/gitlab/gitlab_install.sh
+++ charts/gitlab/gitlab_install.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
-helm -n gitlab upgrade --install gitlab gitlab/gitlab \
+helm -n gitlab upgrade --install \
   --timeout 600s \
   --set certmanager-issuer.email=ENTET_YOUR_EMAIL@example.com \
   --set certmanager.install=false \
@@ -13,10 +13,11 @@ helm -n gitlab upgrade --install gitlab gitlab/gitlab \
   --set gitlab-runner.runners.privileged=true \
   --set global.edition=ce \
   --set global.hosts.domain=gitlab.local \
-  --set global.hosts.externalIP=ENTER_YOUR_IP
+  --set global.hosts.externalIP=ENTER_YOUR_IP \
   --set global.hosts.https=false \
   --set global.ingress.class=nginx \
   --set global.kas.enabled=false \
   --set global.shell.port=2222 \
   --set nginx-ingress.enabled=false \
   --set prometheus.install=false \
+  gitlab gitlab/gitlab
